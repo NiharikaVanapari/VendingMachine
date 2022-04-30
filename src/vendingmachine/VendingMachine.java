f23f34e26d8a0463487d3c1b08339c9ea6e323b8
NiharikaVanapari <niharikavanapari23@gmail.com>
2022-04-30 11:00:08 +0530

    Vending Machine

---
+++ src/vendingmachine/VendingMachine.java
@@ -0,0 +1,31 @@
+package vendingmachine;
+import java.util.*;
+import java.lang.*;
+public class VendingMachine {
+	public static void countCurrency(int amount) {
+		 int[] notes = new int[]{ 1000, 500, 100, 50, 20, 10, 5, 2, 1 };
+	        int[] noteCounter = new int[9];
+	      
+	        // count notes using Greedy approach
+	        for (int i = 0; i < 9; i++) {
+	            if (amount >= notes[i]) {
+	                noteCounter[i] = amount / notes[i];
+	                amount = amount - noteCounter[i] * notes[i];
+	            }
+	        }
+	        System.out.println("Currency Count ->");
+	        for (int i = 0; i < 9; i++) {
+	            if (noteCounter[i] != 0) {
+	                System.out.println(notes[i] + " : "
+	                    + noteCounter[i]);
+	            }
+	        }
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int amount=565;
+		countCurrency(amount);
+
+	}
+
+}
