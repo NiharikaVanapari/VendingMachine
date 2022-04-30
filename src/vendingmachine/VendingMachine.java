65b31ecdd19e58abe72b597182d67230594878c3
NiharikaVanapari <85484150+NiharikaVanapari@users.noreply.github.com>
2022-04-30 11:01:56 +0530

    Update VendingMachine.java

--- src/vendingmachine/VendingMachine.java
+++ src/vendingmachine/VendingMachine.java
@@ -3,18 +3,18 @@ import java.util.*;
 import java.lang.*;
 public class VendingMachine {
 	public static void countCurrency(int amount) {
-		 int[] notes = new int[]{ 1000, 500, 100, 50, 20, 10, 5, 2, 1 };
-	        int[] noteCounter = new int[9];
+		 int[] notes = new int[]{ 1000, 500, 100, 50, 10, 5, 2, 1 };
+	        int[] noteCounter = new int[8];
 	      
 	        // count notes using Greedy approach
-	        for (int i = 0; i < 9; i++) {
+	        for (int i = 0; i < 8; i++) {
 	            if (amount >= notes[i]) {
 	                noteCounter[i] = amount / notes[i];
 	                amount = amount - noteCounter[i] * notes[i];
 	            }
 	        }
 	        System.out.println("Currency Count ->");
-	        for (int i = 0; i < 9; i++) {
+	        for (int i = 0; i < 8; i++) {
 	            if (noteCounter[i] != 0) {
 	                System.out.println(notes[i] + " : "
 	                    + noteCounter[i]);
